0c109d03381adce8c053b7afb3d237afd4b6905e
miuchan <k737566@gmail.com>
2017-03-25 01:03:13 +0800

    Change port to 80 and log infomation after server started.

--- publicServer.js
+++ publicServer.js
@@ -3,7 +3,12 @@ const serve = require('koa-static')
 const path = require('path')
 
 const app = new Koa()
+const port = 80
+const publicPath = path.join(__dirname,'/dist')
 
-app.use(serve(path.join(__dirname,'../dist')))
+app.use(serve(publicPath))
 
-app.listen(8080)
+app.listen(port)
+
+console.log(`serving ${publicPath}`);
+console.log('listening on ' + port)
