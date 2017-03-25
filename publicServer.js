7f45d6b85da9350c0d3c839cc727df28420dbd6e
miuchan <k737566@gmail.com>
2017-03-25 10:35:22 +0800

    Fix bug.

--- publicServer.js
+++ publicServer.js
@@ -1,14 +1,22 @@
 const Koa = require('koa')
+const router = require('koa-router')()
 const serve = require('koa-static')
 const path = require('path')
+const fs = require('fs')
 
 const app = new Koa()
 const port = 80
 const publicPath = path.join(__dirname,'/dist')
+const html = fs.readFileSync(`${publicPath}/index.html`, 'utf-8')
+
+router.get(/.*/, function* (ctx, next) {
+  this.body = html
+})
 
 app.use(serve(publicPath))
+app.use(router.routes())
 
 app.listen(port)
 
-console.log(`serving ${publicPath}`);
+console.log(`serving ${publicPath}`)
 console.log('listening on ' + port)
